d9609b2f324065926031059eba3deba0dda7df62
Nick Plotnikov <mail@skyward.pro>
2021-02-15 19:13:55 +0300

    fix origins

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-//    originWhitelist: ['http://localhost:3000', 'https://www.bloxstaking.com', 'https://bloxinfra.site.strattic.io', 'https://bloxinfra.preview.strattic.io'],
+    originWhitelist: ['https://www.bloxstaking.com', 'https://bloxinfra.site.strattic.io', 'https://bloxinfra.preview.strattic.io'],
 //    requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
