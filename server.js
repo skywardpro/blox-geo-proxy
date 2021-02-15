3beb9783f9dbb69395297c587a31ff12a6a9cf88
Nick Plotnikov <mail@skyward.pro>
2021-02-15 15:30:19 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const corsProxy = require('cors-anywhere');
+
+// Listen on a specific host via the HOST environment variable.
+const host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable.
+const port = process.env.PORT || 8080;
+
+corsProxy.createServer({
+    originWhitelist: ['https://www.bloxstaking.com/', 'https://bloxinfra.site.strattic.io/', 'https://bloxinfra.preview.strattic.io'],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, () => {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
