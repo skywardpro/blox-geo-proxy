dd969cef8524237f3c12af61ecb31c33a3880a59
Nick Plotnikov <mail@skyward.pro>
2021-02-15 15:46:50 +0300

    remove required headers

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
     originWhitelist: ['https://www.bloxstaking.com/', 'https://bloxinfra.site.strattic.io/', 'https://bloxinfra.preview.strattic.io'],
-    requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
